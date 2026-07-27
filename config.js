8dd1428124542e1c23146657366087ffcb4daf2b
klssameday <info@klssameday.co.uk>
2026-07-27 11:46:03 +0100

    Fix Supabase configuration

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KLS_CONFIG = {
-  SUPABASE_URL: https://mdbgcnctawvqdynkhnhf.supabase.co
-  SUPABASE_ANON_KEY: sb_publishable_Mv4TaxvRpRLxajma4k2j4w_zGMTwwrj
+  SUPABASE_URL: https:"https://mdbgcnctawvqdynkhnhf.supabase.co"
+  SUPABASE_ANON_KEY: "sb_publishable_Mv4TaxvRpRLxajma4k2j4w_zGMTwwrj"
 };

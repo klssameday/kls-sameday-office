9b6c2222ad5c1fc2df0638faeea67d22d07071a0
klssameday <info@klssameday.co.uk>
2026-07-27 11:53:20 +0100

    Fix config property names for Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KLS_CONFIG = {
-  SUPABASE_URL: https:"https://mdbgcnctawvqdynkhnhf.supabase.co"
-  SUPABASE_ANON_KEY: "sb_publishable_Mv4TaxvRpRLxajma4k2j4w_zGMTwwrj"
+supabaseUrl:"https://mdbgcnctawvqdynkhnhf.supabase.co", 
+supabaseAnonKey:"sb_publishable_Mv4TaxvRpRLxajma4k2j4w_zGMTwwrj"
 };

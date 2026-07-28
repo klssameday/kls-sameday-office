05fb7cefd9a7e7fe36f2088f7a49346287075a07
klssameday <info@klssameday.co.uk>
2026-07-28 20:43:29 +0100

    Update sw.js for v35.2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kls-v35-1-0';
+const CACHE = 'kls-v35-2-0';
 const APP_SHELL = [
   '/', '/index.html', '/offline.html', '/styles.css', '/app.js', '/manifest.json',
   '/driver.html', '/driver.css', '/driver.js',

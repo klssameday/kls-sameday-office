b5e2fa1b08af8aa0febaf2b8ba8fe10ff8f8ed85
klssameday <info@klssameday.co.uk>
2026-08-01 15:48:46 +0100

    Refresh app cache for privacy links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kls-v35-4-9';
+const CACHE = 'kls-v35-4-10';
 const APP_SHELL = [
   '/', '/index.html', '/offline.html', '/styles.css', '/public-quote.css', '/app.js', '/manifest.json',
   '/driver.html', '/driver.css', '/driver.js',
